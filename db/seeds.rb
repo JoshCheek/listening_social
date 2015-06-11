34641def003a8a4eaa5a4228a4056d8ff56ad9cf
Josh Cheek <josh.cheek@gmail.com>
2015-06-11 16:11:58 -0600

    Add Some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Playlist.create! do |pl|
+  pl.name = 'Jazz'
+
+  pl.currently_playing = pl.songs.build \
+    title:          "Our Love Is Here To Stay",
+    artist:         "Ella Fitzgerald",
+    soundcloud_url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/189366180&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
+
+  pl.songs.build \
+    title:          "Autumn Leaves",
+    artist:         "Chet Baker",
+    soundcloud_url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/179364153&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
+end
+
+Playlist.create! name: 'Rock'
+
+Playlist.create! name: 'Nu Metal'
